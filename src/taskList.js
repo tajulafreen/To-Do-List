0133a27f359701e272915fa4570d456f681799a3
Mahdi Noori <mahdinoori2389@gmail.com>
2023-06-22 15:13:43 +0430

    Add mocking to the addremove.test.js file

--- src/taskList.js
+++ src/taskList.js
@@ -7,8 +7,8 @@ export default class TaskList {
   }
 
   addTask(description) {
-    const task = new Task(description, false, this.tasks.length + 1);
-    this.tasks.push(task);
+    const task = new Task(description(task), false, this.tasks.length + 1);
+    this.tasks.push
     this.saveTasks();
   }
 
